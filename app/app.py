163c53b5474e860ed50a9ef8d87036ac426e7c0b
anisriva <postanisrivastava@outlook.com>
2025-06-10 15:52:07 +0530

    fix : config changes + view routes

--- app/app.py
+++ app/app.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Depends
 from pydantic import ValidationError
-from app.src.routes import api_router
+from app.src.routes import api_router, view_router
 from app.src.middlewares.error_handler import (
     not_found,
     error_handler,
     validation_error_handler,
 )
 
-from app.src.config import get_app_config
+from app.src.config.app import get_app_config
 from app.src.connectors.db import get_session
 
 fast_api_config = get_app_config()
@@ -21,6 +21,8 @@ app = FastAPI(
 
 # Register routes from router aggregator
 app.include_router(api_router, dependencies=[Depends(get_session)])
+app.include_router(view_router, dependencies=[Depends(get_session)])
+
 # Middlewares
 app.middleware("http")(not_found)
 
