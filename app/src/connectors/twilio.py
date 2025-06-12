c71e541fa73f5b0d5f7a336c3bdf80499bbac1d8
anisriva <postanisrivastava@outlook.com>
2025-06-12 07:54:03 +0530

    fix : default mode for verify -> False

--- app/src/connectors/twilio.py
+++ app/src/connectors/twilio.py
@@ -3,14 +3,14 @@ from app.src.config.twilio import get_twilio_config
 from twilio.rest import Client
 from twilio.http.http_client import TwilioHttpClient
 
-def get_twilio_client():
+def get_twilio_client(verify=False):
     account_sid, auth_token, *_ = get_twilio_config().values()
     
     # Create a custom HTTP client with SSL verification
     http_client = TwilioHttpClient()
     
     # Set SSL certificate verification
-    http_client.session.verify = certifi.where()
+    http_client.session.verify = certifi.where() if verify else False
     
     return Client(
         account_sid,
