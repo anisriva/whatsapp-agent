2428428de9a4b1cdf6f9a3a0a62fe1f7d1ecf043
anisriva <postanisrivastava@outlook.com>
2025-06-12 07:54:28 +0530

    fix : whatsapp compliant messaging

--- app/src/helpers/twilio/whatsapp/__init__.py
+++ app/src/helpers/twilio/whatsapp/__init__.py
@@ -1,13 +1,13 @@
 from app.src.connectors.twilio import get_twilio_client
 from app.src.config.twilio import get_twilio_config
 
-async def send(to_number:str, message_body: str):
+async def send_whatsapp_message(to_number:str, message_body: str):
     client = get_twilio_client()
     bot_number = get_twilio_config()["whatsapp_number"]
     message = client.messages.create(
         body=message_body,
-        from_=bot_number,
-        to=to_number
+        from_=f'whatsapp:{bot_number}',
+        to=f'whatsapp:{to_number}'
     )
     print(message)
     return message.sid
