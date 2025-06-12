e319eef4a77e5661405c44824598c471ed3c8fba
anisriva <postanisrivastava@outlook.com>
2025-06-12 07:54:51 +0530

    fix : send -> send_whatsapp_message

--- app/src/controllers/messages_controller.py
+++ app/src/controllers/messages_controller.py
@@ -1,6 +1,6 @@
 from app.src.models.api import ApiResponse
 from app.src.models.messages import MessageSendRequest
-from app.src.helpers.twilio.whatsapp import send
+from app.src.helpers.twilio.whatsapp import send_whatsapp_message
 
 '''
 $ desc   : send message api
@@ -8,5 +8,5 @@ $ route  : POST /api/v1/send_message
 $ access : PRIVATE
 '''
 async def send_message(message_request : MessageSendRequest):
-    res = await send(message_request.to, message_request.content)
+    res = await send_whatsapp_message(message_request.to, message_request.content)
     return ApiResponse(status="success", message=res)
