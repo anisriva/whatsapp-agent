31e323b95eed6341f27fd6c9fc8fde756817d795
Animesh Srivastava <postanisrivastava@outlook.com>
2025-06-15 09:56:55 +0530

    feature : create_db_and_tables

--- app/src/connectors/db.py
+++ app/src/connectors/db.py
@@ -1,7 +1,10 @@
-from sqlmodel import create_engine, Session
+from sqlmodel import create_engine, Session, SQLModel
 from app.src.config.db import get_db_uri
 
 engine = create_engine(get_db_uri())
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 def get_session():
     with Session(engine) as session:
