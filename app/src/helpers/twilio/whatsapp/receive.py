822b2edfc53379521a58f067120b72ffd7a5dde5
Animesh Srivastava <postanisrivastava@outlook.com>
2025-06-15 09:57:56 +0530

    feature : build message_data

---
+++ app/src/helpers/twilio/whatsapp/receive.py
@@ -0,0 +1,20 @@
+from typing import Any
+from app.src.models.messages import MessageData
+
+
+def create_message_data(form_data: Any) -> MessageData:
+    """
+    Creates a MessageData object from form data, sanitizing the input.
+    """
+    message_data = {
+        "message_sid": form_data.get("MessageSid"),
+        "account_sid": form_data.get("AccountSid"),
+        "from_number": form_data.get("From"),  # Format: whatsapp:+1234567890
+        "to_number": form_data.get("To"),  # Format: whatsapp:+1234567890
+        "body": form_data.get("Body", ""),
+        "num_media": int(form_data.get("NumMedia", 0)),
+        "profile_name": form_data.get("ProfileName"),
+        "wa_id": form_data.get("WaId"),
+    }
+
+    return MessageData(**message_data)
