0830ecce016e37479b5396102f90cdeddef2965b
Animesh Srivastava <postanisrivastava@outlook.com>
2025-06-15 10:00:10 +0530

    feature : added receive message

--- app/src/routes/api_routes/twilio_messages_routes.py
+++ app/src/routes/api_routes/twilio_messages_routes.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 from app.src.controllers.twilio_messages_controller import (
     send_whatsapp_message,
-    send_whatsapp_poll
+    send_whatsapp_poll,
+    receive_whatsapp_message
 )
 
 router = APIRouter(prefix="/twilio")
 
-router.post("/send_message", tags=["message"])(send_whatsapp_message)
-router.post("/send_poll", tags=["message"])(send_whatsapp_poll)
+router.post("/send_message", tags=["messages"])(send_whatsapp_message)
+router.post("/send_poll", tags=["messages"])(send_whatsapp_poll)
+router.post("/receive_message", tags=["messages"])(receive_whatsapp_message)
