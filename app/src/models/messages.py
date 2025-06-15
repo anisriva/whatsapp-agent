0f0b841ec19e3aae66f751389be35cdd52f2f7e8
anisriva <postanisrivastava@outlook.com>
2025-06-15 18:48:47 +0530

    fix : messagedata model optimizations

--- app/src/models/messages.py
+++ app/src/models/messages.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel, Field
-from typing import Optional, List
-from pydantic import BaseModel
-from datetime import datetime
+from typing import Optional
+from datetime import datetime, timezone
+
 
 class SendTextMessageRequest(SQLModel):
     recipient_number: str
@@ -14,12 +14,11 @@ class SendPollMessageRequest(SQLModel):
 
 class MessageData(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    message_sid: str = Field(index=True, nullable=False)
-    account_sid: str = Field(index=True, nullable=False)
-    from_number: str = Field(index=False, nullable=False)
-    to_number: str = Field(index=False, nullable=False)
-    body: str = Field(index=False, nullable=False)
-    num_media: Optional[int] = Field(default=0)
-    profile_name: Optional[str] = None
+    message_sid: str
+    account_sid: str
     wa_id: Optional[str] = None
-    created_at: datetime = Field(default=datetime.utcnow)
+    from_number: str
+    to_number: str
+    body: str
+    profile_name: Optional[str] = None
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
