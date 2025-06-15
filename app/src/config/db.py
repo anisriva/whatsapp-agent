1efe387306bb17d305479b92377ff8f9e021d4bb
anisriva <postanisrivastava@outlook.com>
2025-06-15 22:23:25 +0530

    fix : ignore port if empty

--- app/src/config/db.py
+++ app/src/config/db.py
@@ -5,9 +5,9 @@ def get_db_uri():
     user = get_env_vars("DB_USER", "")
     password = get_env_vars("DB_PASSWORD", "")
     host = get_env_vars("DB_HOST", "")
-    port = get_env_vars("DB_PORT", "")
+    port = get_env_vars("DB_PORT", None)
     dbname = get_env_vars("DB_NAME", "")
     user_info = f"{user}:{password}@" if user and password and db_type != "sqlite" else ""
-    host_info = f"{host}:{port}" if host and port and db_type != "sqlite" else ""
+    host_info = f"{host}:{port}" if host and port is not None and db_type != "sqlite" else f"{host}" if host and db_type != "sqlite" else ""
     db_info = f"/{dbname}.db" if dbname and db_type == "sqlite" else f"/{dbname}" if dbname else ""
     return f"{db_type}://{user_info}{host_info}{db_info}"
