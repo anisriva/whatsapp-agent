c7fa5fc043bd71c155cc52ec76bcffc3498ab445
Animesh Srivastava <postanisrivastava@outlook.com>
2025-06-15 13:18:52 +0530

    fix : save message data

--- app/src/dao/message_data.py
+++ app/src/dao/message_data.py
@@ -1,5 +1,20 @@
+import logging
 from app.src.models.messages import MessageData
+from app.src.connectors.db import get_session
 
-async def save_message(message_data: MessageData):
-    print(f"Got message-> {message_data}")
-    pass
+async def save_message(message_data: MessageData) -> bool:
+    status = False
+    message = ""
+    try:
+        session = get_session()
+        logging.debug(f"Got message : {message_data}")
+        session.add(message_data)
+        session.commit()
+        status = True
+    except Exception as e:
+        session.rollback()
+        message = f"Error saving message: {e}"
+        logging.error(message)
+    finally:
+        session.close()
+        return (status, message)
